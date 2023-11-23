00d63b579c70c9044bb0950819f8b1ba4153d74e
marcosferr <ferreiramarcosadrian@gmail.com>
2023-11-22 23:23:49 -0300

    Add Header component and refactor dependences

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <h1>CV Creator</h1>
+          <p>
+            Made by <a href="www.github.com/u/marcosferr">MarcosFerr</a>
+          </p>
+        </div>
+        <nav>
+          <a href="" className="nav-btn">
+            Edit CV
+          </a>
+          <a href="" className="nav-btn">
+            Preview CV
+          </a>
+          <a href="" className="btn-outline">
+            Fill Example CV
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
